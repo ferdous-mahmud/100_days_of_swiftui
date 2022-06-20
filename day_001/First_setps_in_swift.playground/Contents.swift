fe1ff46bf34e8cd2a5379970a0c91bd89e9738fd
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-06-20 14:14:09 +0600

    first commit

---
+++ day_001/First_setps_in_swift.playground/Contents.swift
@@ -0,0 +1,63 @@
+import UIKit
+
+// Variables
+// Variables vary
+// We can change its value
+var greeting = "Hello, playground"
+greeting = "100 Days of Swift"
+
+var favColor = "Red"
+favColor = "Blue"
+
+// Strings and Integers
+// Strings represent set of characters
+// Intergers are number
+var name: String = "Ferdous Mahmud Akash"
+var age: Int = 21
+
+// Type safe swift
+// Swift not allowed asign one type of value to other types of variable
+var one = 1 // Int
+//one = "one" // String
+
+// Multiple line string
+var mulString: String = """
+This
+is
+Multiple line
+string
+"""
+
+var notMulString: String = """
+This \
+is \
+not \
+multiple line string
+"""
+
+// Double
+// Doubles are the floating point integers
+var pi = 3.1416
+var pie: Double = 3.1416925
+
+// Boolean
+// True or False
+var isLogedIn = false
+var isAlive: Bool = true
+
+// String interpultion
+// Binding string with variable
+var score: Int = 100
+var message: String = "Your score is: \(score)"
+
+// Constanse
+// One time assginable Never change
+let isSunRiseEase = true
+
+
+// Type anotation
+var year: Int = 1982
+var Height: Double = 7.12
+var parameter: String = "Meter"
+
+
