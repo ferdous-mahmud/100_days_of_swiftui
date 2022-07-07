f4ea06340a61cf52bce5dbad225b6fad8102d963
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-07-07 20:37:49 +0600

    WeSplit more fetcher added

---
+++ day_018/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ferdous Mahmud Akash on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
