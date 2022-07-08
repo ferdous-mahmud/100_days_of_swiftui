55979c61bc8c6de3953b8315980fe54d3c4dea68
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-07-08 11:48:56 +0600

    UniConverter all functionality added

--- day_019/UniConverter/UniConverter/ContentView.swift
+++ day_019/UniConverter/UniConverter/ContentView.swift
@@ -12,7 +12,9 @@ struct ContentView: View {
     @State private var currentInput = 0.00
     @State private var inputSelectedUnit = "Celsius"
     @State private var outputSelectedUnit = "Kelvin"
+    @FocusState private var isInputFieldFocused: Bool
     
+    // Stored Properties
     let conversionTypes = ["Temperature", "Length", "Time", "Volume"]
     
     let temperatureUnits = ["Celsius", "Fahrenheit", "Kelvin"]
@@ -20,6 +22,63 @@ struct ContentView: View {
     let timeUnits = ["seconds", "minutes", "hours", "days"]
     let volumeUnits = ["milliliters", "liters", "cups", "pints", "gallons"]
     
+    // MARK: View
+    var body: some View {
+        NavigationView{
+            Form{
+                // Conversion Type Selection
+                Section {
+                    Picker("Tip percentage", selection: $currentSelectionType) {
+                        ForEach (conversionTypes, id: \.self) {
+                            Text($0)
+                        }
+                    }.pickerStyle(.segmented)
+                } header: {
+                    Text("Select your conversion type")
+                }
+                
+                // Input
+                Section{
+                    TextField("Input \(currentSelectionType)", value: $currentInput, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($isInputFieldFocused)
+                    Picker("Select input type", selection: $inputSelectedUnit){
+                        ForEach(selectOptions, id:\.self){
+                            Text($0)
+                        }
+                    }
+                } header: {
+                    Text("Input \(currentSelectionType)")
+                }
+                
+                // Output
+                Section{
+                    Text(convertedOutput, format: .number)
+                    Picker("Select output type", selection: $outputSelectedUnit){
+                        ForEach(selectOptions, id:\.self){
+                            Text($0)
+                        }
+                    }
+                } header: {
+                    Text("Output \(currentSelectionType)")
+                }
+            }
+            .navigationTitle("UniConverter")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar { // Hide keyboard visiblity
+                ToolbarItemGroup (placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        isInputFieldFocused = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+// MARK: Computed Properties
+extension ContentView {
     var selectOptions: [String] {
         if (currentSelectionType == "Temperature") {
             return temperatureUnits
@@ -56,33 +115,122 @@ struct ContentView: View {
         } else if (inputSelectedUnit == "meters" && outputSelectedUnit == "feet"){
             return currentInput * 3.281
         } else if (inputSelectedUnit == "meters" && outputSelectedUnit == "yards"){
-            return (currentInput - 273.15)
+            return currentInput * 1.094
         } else if (inputSelectedUnit == "meters" && outputSelectedUnit == "miles"){
-            return (currentInput - 273.15)
+            return currentInput / 1609
         } else if (inputSelectedUnit == "kilometers" && outputSelectedUnit == "feet"){
-            return (currentInput - 273.15)
+            return currentInput * 3281
         } else if (inputSelectedUnit == "kilometers" && outputSelectedUnit == "yards"){
-            return (currentInput - 273.15)
+            return currentInput * 1094
         } else if (inputSelectedUnit == "kilometers" && outputSelectedUnit == "miles"){
-            return (currentInput - 273.15)
+            return currentInput / 1.609
         } else if (inputSelectedUnit == "feet" && outputSelectedUnit == "yards"){
-            return (currentInput - 273.15)
+            return currentInput / 3
         } else if (inputSelectedUnit == "feet" && outputSelectedUnit == "miles"){
-            return (currentInput - 273.15)
+            return currentInput / 5280
         } else if (inputSelectedUnit == "yards" && outputSelectedUnit == "miles"){
-            return (currentInput - 273.15)
-        } // add oposit of them again
+            return currentInput / 1760 /// new
+        } else if (inputSelectedUnit == "kilometers" && outputSelectedUnit == "meters") {
+            return currentInput * 1000.00
+        } else if (inputSelectedUnit == "feet" && outputSelectedUnit == "meters"){
+            return currentInput / 3.281
+        } else if (inputSelectedUnit == "yards" && outputSelectedUnit == "meters"){
+            return currentInput / 1.094
+        } else if (inputSelectedUnit == "miles" && outputSelectedUnit == "meters"){
+            return currentInput * 1609
+        } else if (inputSelectedUnit == "feet" && outputSelectedUnit == "kilometers"){
+            return currentInput / 3281
+        } else if (inputSelectedUnit == "yards" && outputSelectedUnit == "kilometers"){
+            return currentInput / 1094
+        } else if (inputSelectedUnit == "miles" && outputSelectedUnit == "kilometers"){
+            return currentInput * 1.609
+        } else if (inputSelectedUnit == "yards" && outputSelectedUnit == "feet"){
+            return currentInput * 3
+        } else if (inputSelectedUnit == "miles" && outputSelectedUnit == "feet"){
+            return currentInput * 5280
+        } else if (inputSelectedUnit == "miles" && outputSelectedUnit == "yards"){
+            return currentInput * 1760
+        }
         else {
             return currentInput
         }
     }
     
     var timeOutput: Double {
-        return 0.03
+        if (inputSelectedUnit == "seconds" && outputSelectedUnit == "minutes") {
+            return currentInput / 60
+        } else if (inputSelectedUnit == "seconds" && outputSelectedUnit == "hours"){
+            return currentInput / 3600
+        } else if (inputSelectedUnit == "seconds" && outputSelectedUnit == "days"){
+            return currentInput / 86400
+        } else if (inputSelectedUnit == "minutes" && outputSelectedUnit == "hours"){
+            return currentInput / 60
+        } else if (inputSelectedUnit == "minutes" && outputSelectedUnit == "days"){
+            return currentInput / 1440
+        } else if (inputSelectedUnit == "hours" && outputSelectedUnit == "days"){
+            return currentInput / 24 // new
+        } else if (inputSelectedUnit == "minutes" && outputSelectedUnit == "seconds") {
+            return currentInput * 60
+        } else if (inputSelectedUnit == "hours" && outputSelectedUnit == "seconds"){
+            return currentInput * 3600
+        } else if (inputSelectedUnit == "days" && outputSelectedUnit == "seconds"){
+            return currentInput * 86400
+        } else if (inputSelectedUnit == "hours" && outputSelectedUnit == "minutes"){
+            return currentInput * 60
+        } else if (inputSelectedUnit == "days" && outputSelectedUnit == "minutes"){
+            return currentInput * 1440
+        } else if (inputSelectedUnit == "days" && outputSelectedUnit == "hours"){
+            return currentInput * 24
+        } else {
+            return currentInput
+        }
     }
     
     var volumeOutput: Double {
-        return 0.04
+        if (inputSelectedUnit == "milliliters" && outputSelectedUnit == "liters") {
+            return currentInput / 1000
+        } else if (inputSelectedUnit == "milliliters" && outputSelectedUnit == "cups"){
+            return currentInput / 284.1
+        } else if (inputSelectedUnit == "milliliters" && outputSelectedUnit == "pints"){
+            return currentInput / 568.3
+        } else if (inputSelectedUnit == "milliliters" && outputSelectedUnit == "gallons"){
+            return currentInput / 4546
+        } else if (inputSelectedUnit == "liters" && outputSelectedUnit == "cups"){
+            return currentInput * 3.52
+        } else if (inputSelectedUnit == "liters" && outputSelectedUnit == "pints"){
+            return currentInput * 1.76
+        } else if (inputSelectedUnit == "liters" && outputSelectedUnit == "gallons"){
+            return currentInput / 4.546
+        } else if (inputSelectedUnit == "cups" && outputSelectedUnit == "pints"){
+            return currentInput / 2
+        } else if (inputSelectedUnit == "cups" && outputSelectedUnit == "gallons"){
+            return currentInput / 16
+        } else if (inputSelectedUnit == "pints" && outputSelectedUnit == "gallons"){
+            return currentInput / 8 // new
+        } else if (inputSelectedUnit == "liters" && outputSelectedUnit == "milliliters") {
+            return currentInput * 1000
+        } else if (inputSelectedUnit == "cups" && outputSelectedUnit == "milliliters"){
+            return currentInput * 284.1
+        } else if (inputSelectedUnit == "pints" && outputSelectedUnit == "milliliters"){
+            return currentInput * 568.3
+        } else if (inputSelectedUnit == "gallons" && outputSelectedUnit == "milliliters"){
+            return currentInput * 4546
+        } else if (inputSelectedUnit == "cups" && outputSelectedUnit == "liters"){
+            return currentInput / 3.52
+        } else if (inputSelectedUnit == "pints" && outputSelectedUnit == "liters"){
+            return currentInput / 1.76
+        } else if (inputSelectedUnit == "gallons" && outputSelectedUnit == "liters"){
+            return currentInput * 4.546
+        } else if (inputSelectedUnit == "pints" && outputSelectedUnit == "cups"){
+            return currentInput * 2
+        } else if (inputSelectedUnit == "gallons" && outputSelectedUnit == "cups"){
+            return currentInput * 16
+        } else if (inputSelectedUnit == "gallons" && outputSelectedUnit == "pints"){
+            return currentInput * 8
+        }
+        else {
+            return currentInput
+        }
     }
     
     var convertedOutput: Double {
@@ -96,51 +244,9 @@ struct ContentView: View {
             return volumeOutput
         }
     }
-    
-    var body: some View {
-        NavigationView{
-            Form{
-                // Conversion Type Selection
-                Section {
-                    Picker("Tip percentage", selection: $currentSelectionType) {
-                        ForEach (conversionTypes, id: \.self) {
-                            Text($0)
-                        }
-                    }.pickerStyle(.segmented)
-                } header: {
-                    Text("Select your conversion type")
-                }
-                
-                // Input
-                Section{
-                    TextField("Input \(currentSelectionType)", value: $currentInput, format: .number)
-                    Picker("Select input type", selection: $inputSelectedUnit){
-                        ForEach(selectOptions, id:\.self){
-                            Text($0)
-                        }
-                    }
-                } header: {
-                    Text("Input")
-                }
-                
-                // Output
-                Section{
-                    Text(convertedOutput, format: .number)
-                    Picker("Select input type", selection: $outputSelectedUnit){
-                        ForEach(selectOptions, id:\.self){
-                            Text($0)
-                        }
-                    }
-                } header: {
-                    Text("Output")
-                }
-            }
-            .navigationTitle("UniConverter")
-            .navigationBarTitleDisplayMode(.inline)
-        }
-    }
 }
 
+// MARK: Preview
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
