f3a4b3222a29944c7f217ec376492253634a5e36
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-07-21 01:49:34 +0600

    change app BG

--- day_032/Animations/Animations/ContentView.swift
+++ day_032/Animations/Animations/ContentView.swift
@@ -11,28 +11,32 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
-        Button(){
-            // Do notihing
-        } label: {
-            Image(systemName: "arrow.down")
-                .font(.largeTitle.weight(.bold))
-        }
-        .padding(50)
-        .foregroundColor(.white)
-        .background(.green)
-        .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(.green)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeInOut(duration: 1)
-                        .repeatForever(autoreverses: false),
-                    value: animationAmount
-                )
-        ).onAppear{
-            animationAmount = 2
+        ZStack {
+            Color("appBG")
+            .ignoresSafeArea()
+            Button(){
+                // Do notihing
+            } label: {
+                Image(systemName: "arrow.down")
+                    .font(.largeTitle.weight(.bold))
+            }
+            .padding(50)
+            .foregroundColor(.white)
+            .background(.green)
+            .clipShape(Circle())
+            .overlay(
+                Circle()
+                    .stroke(.green)
+                    .scaleEffect(animationAmount)
+                    .opacity(2 - animationAmount)
+                    .animation(
+                        .easeInOut(duration: 1)
+                            .repeatForever(autoreverses: false),
+                        value: animationAmount
+                    )
+            ).onAppear{
+                animationAmount = 2
+            }
         }
     }
 }
