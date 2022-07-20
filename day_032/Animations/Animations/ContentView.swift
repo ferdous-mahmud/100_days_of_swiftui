901a14eb8926c49486c7f3fa43ae11075f4e39c5
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-07-21 01:14:54 +0600

    download button added

---
+++ day_032/Animations/Animations/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Ferdous Mahmud Akash on 7/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var animationAmount = 1.0
+    
+    var body: some View {
+        Button(){
+            // Do notihing
+        } label: {
+            Image(systemName: "arrow.down")
+                .font(.largeTitle.weight(.bold))
+        }
+        .padding(50)
+        .foregroundColor(.white)
+        .background(.green)
+        .clipShape(Circle())
+        .overlay(
+            Circle()
+                .stroke(.green)
+                .scaleEffect(animationAmount)
+                .opacity(2 - animationAmount)
+                .animation(
+                    .easeInOut(duration: 1)
+                        .repeatForever(autoreverses: false),
+                    value: animationAmount
+                )
+        ).onAppear{
+            animationAmount = 2
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
