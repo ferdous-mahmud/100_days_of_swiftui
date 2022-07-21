51dc138e9597f0c135e092120a978f76ffa9adf4
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-07-22 00:04:23 +0600

    guesture animations added

---
+++ day_033/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Ferdous Mahmud Akash on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
